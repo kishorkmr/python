0547c8a364c952cacf7e77a7c4063a2e17be630c
Kishor Kumar <kishor.cts@gmail.com>
2022-12-30 18:54:36 +0000

    initial commit

---
+++ 0_learn_core_python/20_module.py
@@ -0,0 +1,7 @@
+import test_module
+
+#import 13_weather1   #--    import 13_weather1      ^ SyntaxError: invalid decimal literal
+#The Python "SyntaxError: invalid decimal literal" occurs when we declare a variable with a name that starts with a digit. To solve the error, start the variable name with a letter or an underscore as variable names cannot start with numbers.
+
+
+test_module.main()
